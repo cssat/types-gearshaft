5e54a6a0e806f80bdd773d95f73b3c5169034d5b
Adam Pearson <adam@substantial.com>
2020-04-06 16:53:49 -0700

    Use tsc for watching... and fix some tests

--- tests/entity-store.ts
+++ tests/entity-store.ts
@@ -1,14 +1,19 @@
-import { createEntityStore } from 'gearshaft/entity-store';
-import { MessageStore } from 'gearshaft/message-store';
-
+import { createEntityStore } from '../entity-store';
+import { MessageStore } from '../message-store';
+import { EntityProjection } from '../entity-projection'
 const messageStore: MessageStore = {
     get: a => a + 'foo',
     read: a => a + 'bar',
 };
 
+const projection = {
+  project : 'foo',
+  registerHandlers: 'bar'
+}
+
 createEntityStore({
     category: '',
     entity: Object,
-    projection: {},
+    projection,
     messageStore,
 });
