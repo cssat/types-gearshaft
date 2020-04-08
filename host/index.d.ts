e8eb176d63438539beb58066972314b13255e9a4
Adam Pearson <adam@substantial.com>
2020-04-08 14:57:08 -0700

    Host extends EventEmitter since Gearshaft v0.13.1

--- host/index.d.ts
+++ host/index.d.ts
@@ -1,10 +1,10 @@
-import { Runner, Consumer } from '../consumer';
-
-type Host = {
+import { Consumer } from '../consumer';
+import { EventEmitter } from 'events';
+interface Host extends EventEmitter {
   pause: () => Promise<void>;
   stop: () => Promise<void>;
   unpause: () => void;
-};
+}
 
 type RegisterFn = (consumer: Consumer) => void; // TODO: will be a consumer type
 type Register = { register: RegisterFn };
