dd4fece87b57e5cacede219faef4757520507894
Elsa Moluf <elsa@substantial.com>
2020-04-09 18:13:21 -0700

    createDocumentProjection return is typed

--- document-projection/index.d.ts
+++ document-projection/index.d.ts
@@ -12,4 +12,15 @@ type CreateDocumentProjectionParams<M, E> = {
   projection: EntityProjection;
 };
 
-export function createDocumentProjection<M, E>(options: CreateDocumentProjectionParams<M, E>): void;
+type ConsumerHandlerFunc<T> = (message: T) => void;
+type RegisterFn = (register: <T>(messageType: GenericConstructor<T>, handler: ConsumerHandlerFunc<T>) => void) => void;
+
+type CreateDocumentProjectionReturn<T> = {
+  documentStore: any;
+  registerHandlers: RegisterFn;
+  handler: ConsumerHandlerFunc<T>;
+};
+
+export function createDocumentProjection<M, E, T>(
+  options: CreateDocumentProjectionParams<M, E>,
+): CreateDocumentProjectionReturn<T>;
