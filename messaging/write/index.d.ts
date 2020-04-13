08518a80d7153716ef5c9d86a807758311bf5b63
Elsa Moluf <elsa@substantial.com>
2020-04-13 01:31:19 -0700

    Writer is typed

--- messaging/write/index.d.ts
+++ messaging/write/index.d.ts
@@ -1,12 +1,19 @@
 import { Log } from '../../logging';
 import { MessageStore } from '../../message-store';
 import { Message } from '../../.';
+import { ClassLike } from '../../.';
 
 type CreateWriterOptions = {
   log?: Log;
   messageStore: MessageStore;
 };
 
-type WriteFn = (message: Message, streaName: string) => number;
+type WriteFn = (message: Message, streamName: string, options?: { expectedVersion?: number }) => Promise<number>;
 
-export function createWriter(options: CreateWriterOptions): WriteFn;
+export type Writer = {
+  write: WriteFn;
+  emitter: ClassLike;
+  initial: (message: Message, streamName: string) => Promise<number>;
+};
+
+export function createWriter(options: CreateWriterOptions): Writer;
