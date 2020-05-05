ca21bf8c45d2419c8214749981747b22ce5214f2
Kelly Towle <kelly.towle@substantial.com>
2020-05-04 18:20:04 -0700

    Metadata type added

--- messaging/index.d.ts
+++ messaging/index.d.ts
@@ -1,3 +1,13 @@
 export function follow<T, U>(previous: T, classOfNext: new (...args: any[]) => U, additionalFields?: Partial<U>): U;
 
+export interface Metadata {
+  follow: Function;
+  globalPosition: number;
+  position: number;
+  causationMessageStreamName?: string;
+  causationMessagePosition?: number;
+  causationMessageGlobalPosition?: number;
+  correlationStreamName?: string;
+  replyStreamName?: string;
+}
 export * from './write';
